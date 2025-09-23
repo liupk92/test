1ea77ac3ad4f81d2c592017fefb054ba02f7e077
liupk92 <36259675+liupk92@users.noreply.github.com>
2025-09-23 17:18:47 +0800

    删掉后面两端main.c

--- main.c
+++ main.c
@@ -67,56 +67,6 @@ uint8_t SPI1Transfer(uint8_t data)
 
 
 
-//#include "TM7707.h"
-///* SPI3初始化 */
-//void SPI3Init(void)
-//{
-//    GPIO_InitTypeDef GPIO_InitStructure;
-//    SPI_InitTypeDef  SPI_InitStructure;
-
-//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE );   //使能GPIOB时钟
-//    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3,  ENABLE );   //使能SPI2时钟
-//	
-//		RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
-//		GPIO_PinRemapConfig(GPIO_Remap_SPI3,ENABLE);
-//		GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE); 
-//			
-//	
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_12; //初始化SPI2引脚PB13->SCK,PB15->MOSI
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;          //设置为复用推挽输出
-//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;        //设置输出速率为50MHz
-//    GPIO_Init(GPIOC, &GPIO_InitStructure);                   //初始化GPIOB
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;               //初始化SPI2引脚PB14->MISO
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;    //设置为浮空输入
-//    GPIO_Init(GPIOC, &GPIO_InitStructure);                   //初始化GPIOB
-
-//    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //SPI设置为双线双向全双工
-//    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;                      //设置SPI为主机模式
-//    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;                  //设置8位数据格式
-//    SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;                        //时钟空闲状态为高电平
-//    SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;                       //时钟第二个边沿沿进行数据锁存(被读取)
-//    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;                          //NSS引脚信号由SSI位控制
-//    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64; //波特率预分频值为2,波特率为APB1总线频率36M/2=18M
-//    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;                 //数据传输为高位在前
-//    SPI_InitStructure.SPI_CRCPolynomial = 7;                           //CRC值计算的多项式(实际未使用)
-//    SPI_Init(SPI3, &SPI_InitStructure);                                //初始化外设SPI2
-//    SPI_Cmd(SPI3, ENABLE);                                             //使能SPI2
-//}
-
-///* SPI1数据传输，data-发送的数据;返回值-接收到的数据*/
-//uint8_t SPI3Transfer(uint8_t data)                       //使用寄存器操作以提供整体的数据传输速率
-//{
-//	CS_0();
-//    while ((SPI3->SR & SPI_I2S_FLAG_TXE) == 0);  //等待发送完成
-//    SPI3->DR = data;                             //写数据到发送缓冲区
-//    while ((SPI3->SR & SPI_I2S_FLAG_RXNE) == 0); //等待接收缓冲区非空，表示移位寄存器将数据发送完毕     
-//   	CS_1();
-//	return SPI3->DR;                             //返回接收到的数据  
-//}
-
-
-
-
 
 
 
